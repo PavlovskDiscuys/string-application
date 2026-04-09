dee00b283a8b52e6de560f6cfd1edc9559cefea9
Pav <robsarmiento@icloud.com>
2026-04-09 15:16:48 -0500

    Reciept completed

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,87 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        // Question 1:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        // 1. Declare a variable to store your favorite color
+
+        String favoriteColor = "pink";
+        System.out.println(favoriteColor);
+
+        // 2. Declare a variable to store the year you started this class
+
+        int startYear = 2026;
+        System.out.println(startYear);
+
+        // 3. Declare a variable to store your middle initial
+
+        String middleInitial = "J";
+        System.out.println(middleInitial);
+
+
+        // 4. Declare a variable to store whether you have pets (yes/no)
+
+        boolean havePets = false;
+        System.out.println(havePets);
+
+        // 5. Declare a variable with a nice message or saying
+
+        String niceMessage = "You can do it I believe in you.";
+        System.out.println(niceMessage);
+
+        //-----------------------------------------------------------------------------
+
+        //Step 2
+        // Declare AND initialize variables for:
+
+
+
+        // 1. The number of days in a week
+
+        short weekDays = 7;
+
+        // 2. The price of coffee ($4.99)
+
+        double coffeePrice = 4.99;
+
+        // 3. Your favorite letter
+
+        String favoriteLetter = "H";
+
+        // 4. Whether it's raining (true/false)
+
+        boolean isRaining = false;
+
+        // 5. Print all values in complete sentences
+        System.out.println("There are " + weekDays + "days in a week");
+        System.out.println("The coffee is $" + coffeePrice + " today");
+        System.out.println("My favorite letter is " + favoriteLetter + ".");
+        System.out.println("Is it raining? " + isRaining);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
