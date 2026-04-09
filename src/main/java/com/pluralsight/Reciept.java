55a3ea858d21bdefa331a3f12fea412d9d19dc3c
Pav <robsarmiento@icloud.com>
2026-04-09 15:17:29 -0500

    Reciept completed

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] args) {
+
+        //Energy drink
+        String energyDrink = "Matcha latte";
+
+        //Item price
+        double itemPrice = 7.99;
+
+        //Quantity variable
+        int quantityVariable = 1;
+
+
+        System.out.println("You have bought " + quantityVariable + " " + energyDrink + " $" + itemPrice + ".");
+
+
+    }
+}
+
